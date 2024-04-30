8995e1ae5b4e888fb69f52fb9fc9f8d35f7d9355
Melly Kurokawa <158236994+MellyKurokawa@users.noreply.github.com>
2024-04-29 18:30:26 -0700

    ListaDesordenada_GallegosMelissa

---
+++ ShuffleFunction/ShuffleFunction.py
@@ -0,0 +1,11 @@
+#Importamos la librería random
+import random
+
+# Creamos la lista
+letters = ['a', 'b', 'c']
+
+#Usamos esta utiliza para aleatorizar el orden de los elementos en una lista
+random.shuffle(letters)
+
+#Imprimimos la lista
+print(letters)
