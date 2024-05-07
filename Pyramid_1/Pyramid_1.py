de83c55e7a78ba3c7ea740454ef0f60877be79a1
Melly Kurokawa <158236994+MellyKurokawa@users.noreply.github.com>
2024-05-06 18:28:33 -0700

    PirámideAsterísco_Ejercicio1_06/05/24

---
+++ Pyramid_1/Pyramid_1.py
@@ -0,0 +1,10 @@
+#Pedimos al usuario que ingrese el tamaño para la pirámide
+pyramid = int(input("Ingrese el tamaño que desea de la pirámide: "))
+
+#te da todos los números desde 1 hasta pyramid
+for height in range(1, pyramid + 1):
+    print('*' * height)# imprimirá asteriscos por el valor de height 
+
+
+
+    
